f2125b6626acfcd0c4dd132dcaf501200f4ed9f8
user <user@users-MacBook-Pro.local>
2022-09-07 18:15:52 +0900

    feat: add header with signup/login form

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,14 +1,41 @@
 import React, {useEffect, useState} from "react";
+import Signup from './Signup';
+import Login from './Login';
 
 require('./Header.css');
 
-export default function Header()
-{
+export default function Header() {
+    const [showSignupModal, setShowSignupModal] = React.useState(false);
+    const [showLoginModal, setShowLoginModal] = React.useState(false);
     return (
-        <section id={"container"}>
-            <div id={"logo"}>TomoKuru</div>
-            <div id={"login"}>Log in</div>
-            <div id={"signup"}>Sign up</div>
-        </section>
+        <>
+            <section id={"container"}>
+                <div id={"logo"}>TomoKuru</div>
+                <div id={"login"} onClick={() => setShowLoginModal(true)}>
+                    Log in
+                </div>
+                <div id={"signup"} onClick={() => setShowSignupModal(true)}>
+                    Sign up
+                </div>
+            </section>
+            <section>
+                {showLoginModal ? (
+                    <>
+                        <Login
+                            setShowLoginModal={setShowLoginModal}
+                        />
+                    </>
+                ) : null}
+            </section>
+            <section>
+                {showSignupModal ? (
+                    <>
+                        <Signup
+                            setShowSignupModal={setShowSignupModal}
+                        />
+                    </>
+                ) : null}
+            </section>
+        </>
     )
 }
