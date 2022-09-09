8626a5444bc1c86a80aac219e28a2c39fcfd22e2
user <user@users-MacBook-Pro.local>
2022-09-09 15:33:12 +0900

    feat: show vendor profile on sidebar

--- react-app/src/components/Dashboard/Vendor.jsx
+++ react-app/src/components/Dashboard/Vendor.jsx
@@ -1,15 +1,20 @@
 import React, {useEffect, useState} from 'react';
+import {UserAuth} from '../../context/AuthContext'
 
 require('./Vendor.css');
 
 export default function Vendor() {
+    const {user} = UserAuth();
     return (
         <>
             <div id={'vendor-container'}>
                 <img id={'vendor-avatar'}
                      src={'https://picsum.photos/100'}
                      alt={"Vendor Avatar"}/>
-                <h1>Vendor</h1>
+                <h1>{user.first_name}</h1>
+                <h1>{user.title}</h1>
+                <h1>City: {user.city_ward}</h1>
+                <h1>Prefecture: {user.prefecture}</h1>
             </div>
         </>
     )
