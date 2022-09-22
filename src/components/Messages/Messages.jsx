d5d6a7ca6e27e2e0df9766839f8d258e1770b076
Jason McRainey <jmcraineydev@gmail.com>
2022-09-22 19:41:14 +0900

    FEAT: add initial file build for messages

---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+import { UserAuth } from "../../../context/AuthContext";
+import { getEventMessages, postEventMessage } from "../../../api";
+
+export default function EventMessages(props) {
+  return <></>;
+}
