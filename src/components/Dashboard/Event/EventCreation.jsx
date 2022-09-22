6783c3c99d7c822665602d2d3aee9f9d4af87ace
ken2020n <64454351+ken2020n@users.noreply.github.com>
2022-09-23 02:08:13 +0900

    feat: add photo edit for event

--- src/components/Dashboard/Event/EventCreation.jsx
+++ src/components/Dashboard/Event/EventCreation.jsx
@@ -115,7 +115,7 @@ export default function EventCreation({setView, setEvents, selectedEventVenue}) 
                     htmlFor="venue-detail-input-photo-url"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
-                    Event Image
+                    Photo
                 </label>
                 <input
                     type="file"
@@ -132,12 +132,6 @@ export default function EventCreation({setView, setEvents, selectedEventVenue}) 
                 >
                     Upload Image
                 </button>
-                {/* <input type="text"
-                       id="venue-detail-input-name"
-                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       value={inputPictureUrl}
-                       onChange={(e) => setInputPictureUrl(e.target.value)}
-                /> */}
             </div>
 
             {/* Save Button */}
