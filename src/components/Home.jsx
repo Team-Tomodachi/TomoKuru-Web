e7cf58e50739b6fdce8f9254e92651cc5917967d
Jason McRainey <jmcraineydev@gmail.com>
2022-09-24 14:04:40 +0900

    FEAT: update image and styling for welcome page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,14 @@ export default function Home() {
     <>
       <div id="bg-image">
         <div className="flex flex-col justify-start content-center h-4/5">
-          <h1 className="text-9xl my-6 drop-shadow-lg" id="product_name">
+          <h1
+            className="text-9xl text-white my-6 drop-shadow-2xl"
+            id="product_name"
+          >
             TomoKuru
           </h1>
           <span
-            className="text-white text-6xl my-6 font-bold drop-shadow-lg"
+            className="text-white text-6xl my-6 font-bold drop-shadow-2xl"
             id="portal"
           >
             Vendor Portal
@@ -19,7 +22,7 @@ export default function Home() {
         </div>
         <br />
         <br />
-        <p className="p-8">Welcome to the vendor portal for Tomokuru.</p>
+        {/* <p className="p-8">Welcome to the vendor portal for Tomokuru.</p> */}
         <br />
         <br />
       </div>
