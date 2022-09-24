8b6f08a96df482b86a31da19a060f8936ff5157c
Jason McRainey <jmcraineydev@gmail.com>
2022-09-24 13:41:12 +0900

    FEAT: add ability to send messages in chat

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { UserAuth } from "../../context/AuthContext";
 import { firestore, storage } from "../../firebase";
 import {
-  getFirestore,
   collection,
   addDoc,
   query,
@@ -17,21 +16,20 @@ import {
 //import { getEventMessages, postEventMessage } from "..TomoKuru-Mobile/../../../api";
 import { getDownloadURL, ref } from "firebase/storage";
 
-const user = UserAuth;
-
-const defaultFileRef = ref(storage, "users/new-users.png");
-
 export default function EventMessages({ group_id, event_id }) {
+  const { user } = UserAuth();
+  const defaultFileRef = ref(storage, "users/new-users.png");
   //STATES
-
+  const [newMessage, setNewMessage] = useState("");
   const [collectionName, setCollectionName] = useState("");
   const [recentMessages, setRecentMessages] = useState([]);
 
   //useEffects
+
   useEffect(() => {
     const name = group_id ? `group_${group_id}` : `event_${event_id}`;
     setCollectionName(name);
-  }, [collectionName, event_id, group_id]);
+  }, [event_id, group_id]);
 
   useEffect(() => {
     if (collectionName) {
@@ -53,26 +51,48 @@ export default function EventMessages({ group_id, event_id }) {
     );
     onSnapshot(recentMessagesQuery, function (snapshot) {
       snapshot.docChanges().forEach(function (change) {
-        console.log(`CHANGE: ${JSON.stringify(change)}`);
+        //console.log(`CHANGE: ${JSON.stringify(change)}`);
         let dbMessage = change.doc.data();
         console.log(`DBMESSAGE: ${JSON.stringify(dbMessage)}`);
         setRecentMessages([...recentMessages, dbMessage]);
+        console.log(
+          `RECENT MESSAGES AFTER CHANGE: ${JSON.stringify(recentMessages)}`
+        );
       });
     });
   };
 
   const saveMessage = async (newMessage) => {
+    console.log("***************");
+    console.log(user);
+    console.log(`NEWMESSAGE: ${newMessage}`);
+    console.log(`FIRST_Name: ${user.first_name}`);
+    console.log(`USER_ID: ${user.id}`);
+    console.log(`PROFILEIMAGE: ${user.photo_url}`);
+    console.log("***************");
+
     try {
       await addDoc(collection(firestore, collectionName), {
-        message: newMessage,
-        user_name: user.first_name,
-        user_id: user.id,
-        profileImage: user.photo_url || "",
+        message: newMessage || "UNDEFINED TEST ENTRY",
+        user_name: user.first_name || "UNDEFINED USER FIRST NAME",
+        user_id: user.id || "UNDEFINED USER ID",
+        profileImage: user.photo_url || "users/new-users.png",
         timestamp: serverTimestamp(),
       });
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`NEW MESSAGE ENTERED: ${newMessage}`);
+    saveMessage(newMessage);
+  };
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setNewMessage(e.target.value);
   };
 
   const getUserImage = async (profileImage) => {
@@ -95,22 +115,36 @@ export default function EventMessages({ group_id, event_id }) {
       <div>
         <div className="message_window" id="message_window">
           {console.log(`.MESSAGES${JSON.stringify(recentMessages)}`)}
-          {
-            <div className="message_card">
-              <p className="message_text">{recentMessages.message}</p>
-              <p className="messare_user">{recentMessages.user_name}</p>
-              <img
-                src={getUserImage(recentMessages.profileImage)}
-                alt="user profile"
-              />
-            </div>
-          }
+          {recentMessages.map((message) => {
+            if (message.profileImage) {
+              return (
+                <div className="message_card">
+                  <p className="message_text">{message.message}</p>
+                  <p className="messare_user">{message.user_name}</p>
+                  <img
+                    src={getUserImage(message.profileImage)}
+                    alt="user profile"
+                  />
+                </div>
+              );
+            }
+            return (
+              <div className="message_card">
+                <p className="message_text">{message.message}</p>
+                <p className="messare_user">{message.user_name}</p>
+              </div>
+            );
+          })}
         </div>
         <div className="message_input">
-          <form action="">
-            <input type="text" placeholder="enter your message here" />
-
-            <button>Send</button>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              type="text"
+              name="newmessage"
+              placeholder="enter your message here"
+              onChange={onChange}
+            />
+            <button type="submit">Send</button>
           </form>
         </div>
       </div>
